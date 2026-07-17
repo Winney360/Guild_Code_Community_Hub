b34edb003ccd0ae397031f7f1f931ac670cad04f
Winfred Nkatha <nkathawinnie94@gmail.com>
2026-07-17 23:23:40 +0300

    feat: integrate cookieParser and authRoutes into express application

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,29 +1,36 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { connectDB } from './config/db.ts';
+import cookieParser from 'cookie-parser';
+import { connectDB } from './config/db.js';
+import authRoutes from './routes/authRoutes.js';
 
-//load environment variales
+// Load environment variables
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-//Connect to Database
+// Connect to Database
 connectDB();
 
-//niddleware
+// Middleware
 app.use(cors({
-    origin : 'http://localhost:5173',
-    credentials : true
+  origin: 'http://localhost:5173', // Vite default port
+  credentials: true
 }));
+app.use(express.json());
+app.use(cookieParser());
 
-//health Check Route
-app.get('api/health', (req,res) => {
-    res.json({status:'ok', message: 'Guild Code backend is running!'});
+// Mount Routes
+app.use('/api/auth', authRoutes);
+
+// Health Check Route
+app.get('/api/health', (req, res) => {
+  res.json({ status: 'ok', message: 'Guild Code backend is running!' });
 });
 
-//start server
-app.listen(PORT, () =>{
-    console.log(`Server is running on port ${PORT}`);
-})
+// Start Server
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on port ${PORT}`);
+});
