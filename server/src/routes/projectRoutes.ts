65c943b384f42367f1117b0602ebfe8c6b62cff9
Winfred Nkatha <nkathawinnie94@gmail.com>
2026-07-19 15:37:51 +0300

    feat: map project CRUD routes sequentially

--- server/src/routes/projectRoutes.ts
+++ server/src/routes/projectRoutes.ts
@@ -1,11 +1,23 @@
 import { Router } from 'express';
-import { getProjects, getProjectById, toggleProjectLike } from '../controllers/projectController.js';
+import {
+  getProjects,
+  getMyProjects,
+  getProjectById,
+  createProject,
+  updateProject,
+  deleteProject,
+  toggleProjectLike,
+} from '../controllers/projectController.js';
 import { protect } from '../middlewares/authMiddleware.js';
 
 const router = Router();
 
 router.get('/', getProjects);
+router.get('/my', protect, getMyProjects);
+router.post('/', protect, createProject);
 router.get('/:id', getProjectById);
+router.patch('/:id', protect, updateProject);
+router.delete('/:id', protect, deleteProject);
 router.post('/:id/like', protect, toggleProjectLike);
 
 export default router;
