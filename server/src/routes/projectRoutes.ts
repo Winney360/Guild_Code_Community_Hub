c8de251f0bb52624fa6bca62fbd28256116a8c08
Winfred Nkatha <nkathawinnie94@gmail.com>
2026-07-19 14:40:26 +0300

    feat: register toggle like endpoint in projectRoutes

--- server/src/routes/projectRoutes.ts
+++ server/src/routes/projectRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { getProjects, getProjectById } from '../controllers/projectController.js';
+import { getProjects, getProjectById, toggleProjectLike } from '../controllers/projectController.js';
+import { protect } from '../middlewares/authMiddleware.js';
 
 const router = Router();
 
 router.get('/', getProjects);
 router.get('/:id', getProjectById);
+router.post('/:id/like', protect, toggleProjectLike);
 
 export default router;
