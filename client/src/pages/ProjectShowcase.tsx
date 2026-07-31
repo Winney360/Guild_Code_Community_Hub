883d9a9882d9344692149bb808d7d635c8fff559
Winfred Nkatha <nkathawinnie94@gmail.com>
2026-07-31 11:08:02 +0300

    Remove unused Link import in ProjectShowcase

--- client/src/pages/ProjectShowcase.tsx
+++ client/src/pages/ProjectShowcase.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext.js';
 import ScrollReveal from '../components/ScrollReveal.js';
 
